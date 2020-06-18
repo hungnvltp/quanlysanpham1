e7d013298a1fdc7c97d189be0be2d1ceac470975
Hung Nguyen <hungnv8.it@gmail.com>
2020-06-18 13:59:55 +0700

    ggggg

--- QuanLySanPham.js
+++ QuanLySanPham.js
@@ -1,4 +1,3 @@
-
 document.getElementById('formLogIn').style.display = "none";
 function moFormDangNhap() {
     document.getElementById('formLogIn').style.display = "block";
@@ -11,7 +10,6 @@ function moFormDangNhap() {
     document.getElementById('idCompany').value = "";
 }
 function idSubmit() {
-
     let idName = document.getElementById('idName').value;
     let idId = document.getElementById('idId').value;
     let idPosition = document.getElementById('idPosition').value
@@ -25,55 +23,40 @@ function idSubmit() {
         // return;
     } else if (idName.length <= 2) {
         alert("Họ và tên không được nhỏ quá 2 kí tự");
-
     } else if (idName.length >= 30) {
         alert("Họ và tên không được lớn hơn 30 kí tự");
-
     }
-
     // phone number
     if (_.isEmpty(idPosition)) {
         alert("Vui lòng nhập số điện thoại");
-
     } else if (idPosition.length <= 2) {
         alert("Số điện thoại không được nhỏ quá 2 kí tự");
-
     } else if (idPosition.length >= 13) {
         alert("Số Điện thoại không được lớn hơn 13 kí tự");
     }
-
     // age
     if (_.isEmpty(idAge)) {
         alert("Vui lòng nhập tuổi");
-
     } else if (idAge.length > 4) {
         alert("Tuổi không được lớn hơn 44444 kí tự");
     }
-
     // tên công ty
     if (_.isEmpty(idCompany)) {
         alert("Vui lòng nhập Tên Công ty");
-
     } else if (idCompany.length <= 2) {
         alert("Tên Công Ty không được nhỏ quá 2 kí tự");
-
     }
     else if (idCompany.length >= 30) {
         alert("Tên Công Ty không được lớn hơn 30 kí tự");
     }
-
     // chức cụ
     if (_.isEmpty(idPosition)) {
         alert("Vui lòng nhập chức vụ");
-
     } else if (idCompany.length >= 30) {
         alert("chức vụ không được lớn hơn 30 kí tự");
     }
-
-
     // lưu thông tin nhân viên 
     var Member = localStorage.getItem('Member') ? JSON.parse(localStorage.getItem('Member')) : [];
-
     Member.push({
         idName: idName,
         idId: idId,
@@ -133,7 +116,6 @@ function renderListMember(data = null) {
 // xóa
 function deleteMember(id) {
     let Member = localStorage.getItem('Member') ? JSON.parse(localStorage.getItem('Member')) : [];
-
     Member.splice(id, 1);
     localStorage.setItem('Member', JSON.stringify(Member));
     renderListMember();
@@ -142,34 +124,26 @@ function deleteMember(id) {
     document.getElementById('idPosition').value = "";
     document.getElementById('idAge').value = "";
     document.getElementById('idCompany').value = "";
-
 }
 // sửa 
 var currIndex;
-
 function editMember(index) {
-
     currIndex = index;
     document.getElementById('update').style.display = "none";
     document.getElementById('sua').style.display = "block";
     document.getElementById('formLogIn').style.display = "block";
     let valueNew = Member[index];
-
-
     document.getElementById('idName').value = valueNew.idName
     document.getElementById('idId').value = valueNew.idId
     document.getElementById('idPosition').value = valueNew.idPosition
     document.getElementById('idCompany').value = valueNew.idCompany
     document.getElementById('idAge').value = valueNew.idAge
-
-
 }
 function idSua() {
     var ChangeItem = newValue();
     Member[currIndex] = ChangeItem;
     localStorage.setItem('Member', JSON.stringify(Member));
     renderListMember();
-
     document.getElementById('idName').value = "";
     document.getElementById('idId').value = "";
     document.getElementById('idPosition').value = "";
@@ -193,14 +167,11 @@ function search(value) {
     if (value == '') {
         renderListMember();
     } else {
-        let result = Member.filter(Member =>
-            Member.idName.startsWith(value)
+        let result = Member.filter(Member => Member.idName.startsWith(value)
             || Member.idPosition.match(value)
             || Member.idId.includes(value)
             || Member.idCompany.includes(value)
             || Member.idAge.match(value));
-
-
         renderListMember(result ?? []);
     }
 }
